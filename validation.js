df672176a5220cf35a63258eb186a0175f0c3e26
Devloperog <135986012+Devloperog@users.noreply.github.com>
2023-08-15 00:08:35 +0530

    validation.js

--- validation.js
+++ validation.js
@@ -1,11 +1,12 @@
 function sendVerificationCode() {
     var email = document.getElementById("email").value;
-    // Send verification code to the user's email (not shown here)
+    // Simulate sending verification code (not shown here)
     document.getElementById("verification_code_div").style.display = "block";
 }
 
 function verifyEmail() {
     var verificationCode = document.getElementById("verification_code").value;
-    // Verify the code entered by the user (not shown here)
+    // Simulate verification (not shown here)
     return true; // Return true if verification is successful
 }
+
